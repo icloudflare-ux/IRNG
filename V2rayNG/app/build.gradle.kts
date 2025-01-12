77f84c8b0519eecb008ba313342229306704d1ff
icloudflare <cloudflarei388@gmail.com>
2025-01-12 06:33:29 +0330

    Update build.gradle.kts
    1.8.40

--- V2rayNG/app/build.gradle.kts
+++ V2rayNG/app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 21
         targetSdk = 34
         versionCode = 585
-        versionName = "1.8.39"
+        versionName = "1.8.40"
         multiDexEnabled = true
         splits {
             abi {
